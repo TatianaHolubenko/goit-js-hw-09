190f6ccdcce909f31bedafa3055058d01f4362ae
[TatianaHolubenko] <tatiana.holubenko01@gmail.com>
2023-12-24 15:57:34 +0200

    add

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,9 +2,6 @@ const form = document.querySelector('.feedback-form');
 const input = document.querySelector('input');
 const textarea = document.querySelector('textarea');
 
-// const email = input.value;
-// const message = textarea.value;
-
 let formObject = {
   email: '',
   message: '',
@@ -13,36 +10,37 @@ let formObject = {
 const getInfoForm =
   JSON.parse(localStorage.getItem('feedback-form-state')) || {};
 
-function reloadPage() {
+fillInfoForm();
+
+function fillInfoForm() {
   if (getInfoForm) {
-    input.value = getInfoForm.email;
-    textarea.value = getInfoForm.message;
+    input.value = getInfoForm.email || '';
+    textarea.value = getInfoForm.message || '';
   }
 }
 
 form.addEventListener('input', event => {
   const nodeName = event.target.nodeName;
-  console.log(nodeName);
 
   if (nodeName === 'INPUT' || nodeName === 'TEXTAREA') {
     formObject = {
       email: input.value.trim(),
-      message: input.value.trim(),
+      message: textarea.value.trim(),
     };
 
     localStorage.setItem('feedback-form-state', JSON.stringify(formObject));
   }
 });
-reloadPage();
+
 form.addEventListener('submit', event => {
   event.preventDefault();
 
-  if (input.value === '' || input.value === '') {
+  if (input.value === '' || textarea.value === '') {
     alert('Complete all the ribbons!');
   } else {
-    console.log(submitObject);
+    console.log(formObject);
   }
 
-  form.reset();
   localStorage.removeItem('feedback-form-state');
+  form.reset();
 });
